5e87f1e8f303f3929b9f7bb93e29a45d0e4b28c9
nupellot <>
2022-10-05 21:16:16 +0000

    Реализовал сортировку слов пузырьком

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 // Требуется разработать программу для визуального просмотра оглавлений каталогов файловых систем OS UNIX, которая позволяет определить состав любого доступного каталога. Имя каталога должно передаваться программе через аргумет командной строки ее вызова. В результате обработки оглавления заданного каталога программа должна обеспечивать стандартный вывод списков имен подкаталогов и файлов, которые содержаться в нем. Имена файлов и подкаталогов обоих списков должны перечисляться в словарном порядке в соответствие с кодами ASCI, образующих их символов. Стандартный вывод оглавления данного каталога должен завершаться информационным сообщением, которое идентифицирует число подкаталогов и файлов в нем. Это информационное сообщение должно отображаться через протокол стандартной диагностики. При разработке программы необходимо использовать библиотечные функции просмотра каталогов opendir, readdir, closedir и getcwd, а также системные вызовы chdir, sbrk, stat, write и exit.
@@ -28,12 +29,35 @@ int main(void) {
 		filenames[AmountOfObjects - 1] = info->d_name;
 		printf("%s\n", info->d_name);
 	}
+
+	
+	int AmountOfSwaps;
+	do {
+		AmountOfSwaps = 0;
+		for (int i = 0; i < AmountOfObjects - 1; i++) {
+			int LeftIsBigger = 0;
+			for (int j = 0; j < length(filenames[i]) || j < length(filenames[i + 1]); j++) {
+				if (filenames[i][j] < filenames[i + 1][j]) {
+					break;
+				} else
+				if (filenames[i][j] > filenames[i + 1][j]) {
+					LeftIsBigger = 1;
+				} else continue;
+			}
+			if (LeftIsBigger) {  // Swapping elements.
+				char* TemporaryVariable = filenames[i];
+				filenames[i] = filenames[i + 1];
+				filenames[i + 1] = TemporaryVariable;
+				AmountOfSwaps++;
+			}
+		}
+	} while (AmountOfSwaps != 0);
+	// printf("%d", (int)'a');
 	
 	for (int i = 0; i < AmountOfObjects; i++) {
 		printf("%s\n", filenames[i]);
 	}
 
-	
 	closedir(directory);
 }
 
